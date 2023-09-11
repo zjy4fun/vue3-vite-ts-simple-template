03e28a8d8f0bc43569014c30866e1178c4916fa3
zjy4fun <zjy4fun@gmail.com>
2023-09-11 18:19:25 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+## ref
+
+vue3 + vite + tailwindcss
+
+https://v2.tailwindcss.com/docs/guides/vue-3-vite
---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div style="display: flex; justify-content: center; align-items: center; color: red">Hello Vue3 + Vite + TS</div>
+</template>
+
+<style scoped></style>
